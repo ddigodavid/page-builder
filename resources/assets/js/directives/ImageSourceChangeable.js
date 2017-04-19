2dc3049716dbeb78fcf7bc8ce905655d63e5859d
Rodrigo <roliveira@webeleven.com.br>
2017-04-19 15:35:56 -0300

    [Image Changeable Directive] Possibilidade de remover imagens

--- resources/assets/js/directives/ImageSourceChangeable.js
+++ resources/assets/js/directives/ImageSourceChangeable.js
@@ -5,6 +5,8 @@ export default {
         $el.attr('directive', 'v-image-changeable');
         let $button = $('<button id="btnChangeImageSource" class="btn btn-default teste-button" style="z-index: 9999;">' +
             '<i class="glyphicon glyphicon-cog"></i> Alterar imagem</button>');
+        
+        $el.parent().addClass('min-height-vue-directive');
 
         $el.parent().on('mouseenter', function() {
             $button.appendTo($(this)).show();
